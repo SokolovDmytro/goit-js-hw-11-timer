76a6e99ba2b6afa60a1ceca13a8eb92af5241812
sokolovdmytro <sokolov.dmytro.an@gmail.com>
2020-05-31 11:28:02 +0300

    timer_final

--- task.js
+++ task.js
@@ -4,27 +4,51 @@
 // страницах регистрации событий,
 // во время технического обслуживания и т. д.
 
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+"use strict";
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.refs = {
+      days: document.querySelector(`${selector} span[data-value="days"]`),
+      hours: document.querySelector(`${selector} span[data-value="hours"]`),
+      mins: document.querySelector(`${selector} span[data-value="mins"]`),
+      secs: document.querySelector(`${selector} span[data-value="secs"]`),
+      error: document.querySelector(".error-message")
+    };
+    this.countTime();
+  }
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  countTime() {
+    setInterval(() => {
+      if (this.targetDate <= this.currentDate) {
+        this.refs.error.textContent = ` Error in Target Date of \"${this.selector}"\ `;
+        return;
+      }
+      this.calcTime();
+      this.updateTime();
+    }, 1000);
+  }
 
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+  calcTime() {
+    this.currentDate = Date.now();
+    let time = this.targetDate - this.currentDate;
+    this.days = Math.floor(time / (1000 * 60 * 60 * 24));
+    this.hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    this.mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    this.secs = Math.floor((time % (1000 * 60)) / 1000);
+  }
+
+  updateTime() {
+    this.refs.days.textContent = this.days;
+    this.refs.hours.textContent = this.hours;
+    this.refs.mins.textContent = this.mins;
+    this.refs.secs.textContent = this.secs;
+  }
+}
+
+new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("JUL 22, 2021")
+});
